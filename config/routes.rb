259d90cf00a87ba21ab71fc8d7e1e5399ef94923
LuchianoW <luchianowang@gmail.com>
2025-02-22 00:44:27 -0500

    Added allowed host for Render

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "pokemons#index"   # 首页
+
+  # 指定完整的 URL
   get "/pokemons", to: "pokemons#index"
-  get "/pokemons/:name", to: "pokemons#details", as: "pokemon"
+  get "/pokemons/:name", to: "pokemons#details", as: "pokemon", host: "pokemonforsep759.onrender.com"
 end
+
 
